6857bd14726b86e722371a9057d3c5be24652f2d
Manjur-WD <114661279+Manjur-WD@users.noreply.github.com>
2024-09-02 13:26:43 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // import { defineConfig } from 'vite'
 // import react from '@vitejs/plugin-react'
+// // import compression from 'vite-plugin-compression';
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
@@ -9,45 +10,16 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createBrotliCompress, createGzip } from 'compression';
-import fs from 'fs';
-import path from 'path';
+import compression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/kangarookids",
-  build: {
-    rollupOptions: {
-      plugins: [
-        {
-          name: 'compression',
-          generateBundle(options, bundle) {
-            Object.keys(bundle).forEach((key) => {
-              const file = bundle[key];
-              if (file.fileName.endsWith('.js') || file.fileName.endsWith('.css')) {
-                const source = file.source;
-                
-                // Compress with Gzip
-                const gzip = createGzip();
-                gzip.write(source);
-                gzip.end();
-                const gzipPath = path.join(options.dir, `${file.fileName}.gz`);
-                fs.writeFileSync(gzipPath, gzip.read());
-
-                // Compress with Brotli
-                const brotli = createBrotliCompress();
-                brotli.write(source);
-                brotli.end();
-                const brotliPath = path.join(options.dir, `${file.fileName}.br`);
-                fs.writeFileSync(brotliPath, brotli.read());
-              }
-            });
-          }
-        }
-      ]
-    }
-  }
+  plugins: [
+    react(),
+    compression({
+      algorithm: 'gzip', // Use 'gzip' for Gzip compression
+      threshold: 10240, // Only compress files larger than 10KB
+      ext: '.gz', // Extension for Gzip files
+    }),
+  ],
 });
-
 
